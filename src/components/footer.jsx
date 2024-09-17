cc40c2154e37925b267395286ea9ca8c9547cb3d
pawarvishnuvardhan2153 <pawarvishnuvardhan2153@gmail.com>
2024-09-17 11:33:18 +0530

    First Folder

---
+++ src/components/footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className='bg-slate-800 text-white text-center fixed bottom-0 w-full'>
+            <div className="logo font-bold text-white text-2xl">
+                <span className='text-green-700'>&lt;</span>
+                Pass
+                <span className='text-green-700'>OP/&gt;</span>
+            </div>
+            <div className="flex justify-center items-center font-semibold">Created by Pawar Vishnu Vardhan</div>
+        </div>
+    )
+}
+
+export default Footer
